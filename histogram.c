ea9f278e61e1ed73468a5ca73a17948785f39aa2
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2025-02-03 18:47:36 +0000

    Create histogram.c

---
+++ histogram.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define MAX_WORD_LENGTH 20
+
+int main() {
+    int c, length = 0;  // Initialize length
+    int word_length[MAX_WORD_LENGTH] = {0};
+    int max_count = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            if (length > 0) {  // Only count words with length > 0
+                if (length < MAX_WORD_LENGTH) {
+                    word_length[length]++;
+                } else {
+                    word_length[MAX_WORD_LENGTH - 1]++;  // Count longer words in last bin
+                }
+
+                if (length < MAX_WORD_LENGTH && word_length[length] > max_count) {
+                    max_count = word_length[length];  // Update max_count safely
+                }
+
+                length = 0;  // Reset for next word
+            }
+        } else {
+            length++;  // Increase word length count
+        }
+    }
+
+    // Handle the last word in input
+    if (length > 0) {
+        if (length < MAX_WORD_LENGTH) {
+            word_length[length]++;
+        } else {
+            word_length[MAX_WORD_LENGTH - 1]++;
+        }
+
+        if (length < MAX_WORD_LENGTH && word_length[length] > max_count) {
+            max_count = word_length[length];
+        }
+    }
+
+    // Print horizontal histogram
+    printf("\nHorizontal Histogram of Word Lengths:\n");
+    for (int i = 1; i < MAX_WORD_LENGTH; i++) {  // Start from 1
+        if (word_length[i] > 0) {
+            printf("%2d | ", i);
+            for (int j = 0; j < word_length[i]; j++) {
+                printf("#");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
