262d66cf7dd93485f2b6e87db2938eb84529fab7
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2025-02-04 14:12:17 +0000

    Create frequency.c

---
+++ frequency.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define ASCII_RANGE 128
+
+int main(){
+    int c;
+    int char_freq[ASCII_RANGE] = {0};
+
+    while ((c = getchar()) != EOF){
+        if (c >= 0 && c <= ASCII_RANGE){
+            char_freq[c]++;
+        }
+    }
+
+    printf("\n-------\n");
+    for (int i = 0; i < ASCII_RANGE; i++){
+        if (char_freq[i] > 0){
+             printf(" %c | ", (i >= 32 && i <= 126) ? i : '?');
+             for (int j = 0; i < char_freq[i]; j++){
+                printf("#");
+             }
+             printf(" (%d)\n", char_freq[i]);
+        } 
+    }
+}
