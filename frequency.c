6cda0106c1b17edb6a9d8ce36d2aa7d9f722aad5
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2025-02-09 16:29:13 +0000

    Update frequency.c

--- frequency.c
+++ frequency.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
-#define ASCII_RANGE 128
+#define ASCII_RANGE 128  
 
-int main(){
+int main() {
     int c;
-    int char_freq[ASCII_RANGE] = {0};
+    int char_freq[ASCII_RANGE] = {0};  
 
-    while ((c = getchar()) != EOF){
-        if (c >= 0 && c <= ASCII_RANGE){
+    while ((c = getchar()) != EOF) {
+        if (c >= 0 && c < ASCII_RANGE) { 
             char_freq[c]++;
         }
     }
 
     printf("\n-------\n");
-    for (int i = 0; i < ASCII_RANGE; i++){
-        if (char_freq[i] > 0){
-             printf(" %c | ", (i >= 32 && i <= 126) ? i : '?');
-             for (int j = 0; i < char_freq[i]; j++){
+
+    // Print histogram
+    for (int i = 0; i < ASCII_RANGE; i++) {
+        if (char_freq[i] > 0) {
+            printf(" %c | ", (i >= 32 && i <= 126) ? i : '?');  
+
+            for (int j = 0; j < char_freq[i]; j++) {
                 printf("#");
-             }
-             printf(" (%d)\n", char_freq[i]);
-        } 
+            }
+            printf(" (%d)\n", char_freq[i]);
+        }
     }
+
+    return 0;
 }
